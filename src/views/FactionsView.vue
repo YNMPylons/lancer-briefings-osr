082869657cdee38bbdd358c048d619f0e9e1f063
YNMPylons <156862787+YNMPylons@users.noreply.github.com>
2026-05-18 15:44:20 +0800

    test

--- src/views/FactionsView.vue
+++ src/views/FactionsView.vue
@@ -1,9 +1,9 @@
 <template>
-	<div id="eventsView" :class="{ animate: animateView }" :style="{ 'animation-delay': animationDelay }" class="content-container">
-		<section id="events" :class="{ animate: animate }" class="section-container">
+	<div id="factionsView" :class="{ animate: animateView }" :style="{ 'animation-delay': animationDelay }" class="content-container">
+		<section id="factions" :class="{ animate: animate }" class="section-container">
 			<div class="section-header clipped-medium-backward">
-				<img src="/icons/clockwork.svg" />
-				<h1>BEATS</h1>
+				<img src="/icons/squad.svg" />
+				<h1>FACTIONS</h1>
 			</div>
 			<div class="section-content-container">
 				<div class="events-list-container">
